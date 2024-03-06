a3c0039d5422bb3369a2f8962a2c035248764e5a
Ariel Parra <darkaico@gmail.com>
2024-03-06 20:18:15 +0100

    chore: adding index and main routes

--- keycloak-books-library/config/routes.rb
+++ keycloak-books-library/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 
+  resources :books
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
